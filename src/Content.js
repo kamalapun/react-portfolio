2ac9497bf7be183de238cf3839a42e1c98cf12f1
Kamala Pun <punkamala197@gmail.com>
2023-04-19 23:39:25 -0500

    Initial commit

---
+++ src/Content.js
@@ -0,0 +1,240 @@
+// import images
+import Hero_person from "./assets/images/Hero/person.png";
+
+import figma from "./assets/images/Skills/figma.png";
+import reactjs from "./assets/images/Skills/react.png";
+import python from "./assets/images/Skills/python.png";
+import java from "./assets/images/Skills/java.png";
+import selenium from "./assets/images/Skills/selenium.png";
+import azure from "./assets/images/Skills/azure.png";
+import bootstrap from "./assets/images/Skills/bootstrap.png";
+import mysql from "./assets/images/Skills/mysql.png";
+import wordpress from "./assets/images/Skills/wordpress.png";
+import csharp from "./assets/images/Skills/csharp.png";
+
+import services_logo1 from "./assets/images/Services/logo1.png";
+import services_logo2 from "./assets/images/Services/logo2.png";
+import services_logo3 from "./assets/images/Services/logo3.png";
+
+import project1 from "./assets/images/projects/img1.png";
+import project2 from "./assets/images/projects/img2.png";
+import project3 from "./assets/images/projects/img3.png";
+import person_project from "./assets/images/projects/person.png";
+
+import avatar1 from "./assets/images/Testimonials/avatar1.png";
+import avatar2 from "./assets/images/Testimonials/avatar2.png";
+import avatar3 from "./assets/images/Testimonials/avatar3.png";
+import avatar4 from "./assets/images/Testimonials/avatar4.png";
+
+import Hireme_person from "./assets/images/Hireme/person.png";
+import Hireme_person2 from "./assets/images/Hireme/person2.png";
+
+// import icons from react-icons
+import { GrMail } from "react-icons/gr";
+import { MdArrowForward, MdCall } from "react-icons/md";
+import { BsInstagram, BsLinkedin } from "react-icons/bs";
+import { TbSmartHome } from "react-icons/tb";
+import { BiUser } from "react-icons/bi";
+import { RiServiceLine, RiProjectorLine } from "react-icons/ri";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
+
+export const content = {
+  nav: [
+    {
+      link: "#home",
+      icon: TbSmartHome,
+    },
+    {
+      link: "#skills",
+      icon: BiUser,
+    },
+    {
+      link: "#experience",
+      icon: RiServiceLine,
+    },
+    {
+      link: "#projects",
+      icon: RiProjectorLine,
+    },
+    {
+      link: "#contact",
+      icon: MdOutlinePermContactCalendar,
+    },
+  ],
+  hero: {
+    title: "Developer",
+    firstName: "Kamala",
+    LastName: "Pun",
+    btnText: "Hire Me",
+    image: Hero_person,
+    hero_content: [
+      {
+        count: "2+",
+        text: "Years of Experinse in Web development",
+      },
+      {
+        count: "10+",
+        text: "Projects Worked in my career",
+      },
+    ],
+  },
+  skills: {
+    title: "Skills",
+    subtitle: "MY TOP SKILLS",
+    skills_content: [
+      {
+        name: "Figma",
+        para: "Lorem ipsum text  dummy",
+        logo: figma,
+      },
+      {
+        name: "React js",
+        para: "Lorem ipsum text  dummy",
+        logo: reactjs,
+      },
+      {
+        name: "Python",
+        para: "Lorem ipsum text  dummy",
+        logo: python,
+      },
+      {
+        name: "Java",
+        para: "Lorem ipsum text  dummy",
+        logo: java,
+      },
+      {
+        name: "Selenium",
+        para: "Lorem ipsum text  dummy",
+        logo: selenium,
+      },
+      {
+        name: "Azure",
+        para: "Lorem ipsum text  dummy",
+        logo: azure,
+      },
+      {
+        name: "C#",
+        para: "Lorem ipsum text  dummy",
+        logo: csharp,
+      },
+      {
+        name: "MYSQL",
+        para: "Lorem ipsum text  dummy",
+        logo: mysql,
+      },
+      {
+        name: "Bootstrap",
+        para: "Lorem ipsum text  dummy",
+        logo: bootstrap,
+      },
+      {
+        name: "Wordpress",
+        para: "Lorem ipsum text  dummy",
+        logo: wordpress,
+      },
+    ],
+    icon: MdArrowForward,
+  },
+  services: {
+    title: "Experiences",
+    subtitle: "WHERE I HAVE WORKED",
+    service_content: [
+      {
+        title: "Web Development",
+        para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        logo: services_logo1,
+      },
+      {
+        title: "Graduate Teaching Assistant",
+        para: "In SCSU School of Graduate Studies, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        logo: services_logo2,
+      },
+      {
+        title: "Teaching Assistant",
+        para: "SCSU School of Graduate Studies, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        logo: services_logo3,
+      },
+    ],
+  },
+  Projects: {
+    title: "Projects",
+    subtitle: "MY CREATION",
+    image: person_project,
+    project_content: [
+      {
+        title: "Gym Website",
+        image: project1,
+      },
+      {
+        title: "Social Media web",
+        image: project2,
+      },
+      {
+        title: "Creative Website",
+        image: project3,
+      },
+    ],
+  },
+  Testimonials: {
+    title: "Testimonials",
+    subtitle: "MY CLIENT REVIEWS",
+    testimonials_content: [
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar1,
+        name: "JOHN DOE",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar2,
+        name: "Tom Alex",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar3,
+        name: "Johnny",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar4,
+        name: "ROBBIN",
+      },
+    ],
+  },
+  Hireme: {
+    title: "Hire Me",
+    subtitle: "FOR YOUR PROJECTS",
+    image1: Hireme_person,
+    image2: Hireme_person2,
+    para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+    btnText: "Hire Me",
+  },
+  Contact: {
+    title: "Contect Me",
+    subtitle: "GET IN TOUCH",
+    social_media: [
+      {
+        text: "pun.kamala178@gmail.com",
+        icon: GrMail,
+        link: "mailto:pun.kamala178@gmail.com",
+      },
+      {
+        text: "+91 1234 56778",
+        icon: MdCall,
+        link: "https://wa.me/1234567890",
+      },
+      {
+        text: "KamalaPun",
+        icon: BsLinkedin,
+        link: "https://www.linkedin.com/in/kamala-pun/",
+      },
+    ],
+  },
+  Footer: {
+    text: "All © Copy Right Reserved 2023",
+  },
+};
